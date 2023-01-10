baf3373089573c3bd6623690a0ce1d7df4ec31f7
Musleha <86237540+Musleha1@users.noreply.github.com>
2023-01-10 23:42:01 +0000

    updated layout and comments

--- index.js
+++ index.js
@@ -114,6 +114,13 @@ console.log("Total Months: " + totalMonths);
 
 var netTotal = 0;
 
+for (var i = 0; i < totalMonths; i++) {
+
+    netTotal = netTotal += finances [i] [1];
+
+
+//! Average Changes in Profit/Losses:
+
 var change = [];
 
 var totalChange = 0;
@@ -122,38 +129,117 @@ var averageChange = 0;
 
 var previousProfit = 0;
 
-var greatest = ["", 0];
-
-var least = ["", Number.MAX_SAFE_INTEGER ]
-
-for (var i = 0; i < totalMonths; i++) {
-
-    netTotal = netTotal += finances [i] [1];
     if (i!==0){
-        previousProfit = finances [i-1][1];
+    previousProfit = finances [i-1][1];
     }
     change [i] = finances [i] [1] - previousProfit;
     totalChange += change [i];
 
-    if (change [i] > greatest [1]){
-        greatest = [finances [i] [0], change [i]];
+    averageChange = totalChange/finances.length;
+
+//! Greatest increase in profit:
+
+var greatest = ["", 0];
+
+if  (change [i] > greatest [1]){
+    greatest = [finances [i] [0], change [i]];
     }
-    if (change [i] < least [1]){
-        least = [finances [i] [0], change [i]];
+
+
+//! Greatest decrease in profit:
+
+var least = ["", Number.MAX_SAFE_INTEGER ]
+
+if  (change [i] < least [1]){
+    least = [finances [i] [0], change [i]];
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
 console.log("Total: $" + netTotal);
-
-
-//! Average Changes in Profit/Losses:
-
-averageChange = totalChange/finances.length;
-
-console.log("Average Change :" + averageChange.toFixed(2));
-
-console.log("Greatest Increase in Profits: " + greatest [0] + " " + "($" + greatest[1] + ")") ;
-
+console.log("Average Change: $" + averageChange.toFixed(2));
+console.log("Greatest Increase in Profits: " + greatest [0] + " " + "($" + greatest[1] + ")");
 console.log("Greatest Decrease in Profits: " + least [0] + " " + "($" + least [1] + ")");
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var change = [];
+
+// var totalChange = 0;
+
+// var averageChange = 0;
+
+// var previousProfit = 0;
+
+// var greatest = ["", 0];
+
+// var least = ["", Number.MAX_SAFE_INTEGER ]
+
+// for (var i = 0; i < totalMonths; i++) {
+
+//     netTotal = netTotal += finances [i] [1];
+//     if (i!==0){
+//         previousProfit = finances [i-1][1];
+//     }
+//     change [i] = finances [i] [1] - previousProfit;
+//     totalChange += change [i];
+
+//     if (change [i] > greatest [1]){
+//         greatest = [finances [i] [0], change [i]];
+//     }
+//     if (change [i] < least [1]){
+//         least = [finances [i] [0], change [i]];
+//     }
+// }
+// console.log("Total: $" + netTotal);
+
+
+// //! Average Changes in Profit/Losses:
+
+// averageChange = totalChange/finances.length;
+
+// console.log("Average Change :" + averageChange.toFixed(2));
+
+// console.log("Greatest Increase in Profits: " + greatest [0] + " " + "($" + greatest[1] + ")") ;
+
+// console.log("Greatest Decrease in Profits: " + least [0] + " " + "($" + least [1] + ")");
 
 
 
